bcb89104cc0c4d8168871be60bd14245e4cdedcf
Nicolas Vicente Navarrete Clemente <nicosli@gmail.com>
2016-05-27 01:20:02 -0500

    se sube migration proveedores

--- database/migrations/2016_05_26_225600_create_productos_table.php
+++ database/migrations/2016_05_26_225600_create_productos_table.php
@@ -15,9 +15,16 @@ class CreateProductosTable extends Migration
         Schema::create('productos', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('categoria_id')->unsigned();
-            $table->foreign('categoria_id')->references('id')->on('categorias')->onDelete('cascade');
+            $table->foreign('categoria_id')->references('id')->on('categorias');
             $table->integer('color_id')->unsigned();
             $table->foreign('color_id')->references('id')->on('colores');
+            //$table->integer('proveedor_id')->unsigned();
+            //$table->foreign('proveedor_id')->references('id')->on('proveedores'); 
+            
+            $table->decimal('costo', 5, 2);
+            $table->decimal('precio_publico', 5,2);
+            $table->integer('talla');
+            $table->string('descripcion');
             $table->timestamps();    
         });
     }
---
+++ database/migrations/2016_05_27_060550_create_proveedores_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProveedoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('proveedores', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('nombre', 50);
+            $table->char('telefono', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('proveedores');
+    }
+}
