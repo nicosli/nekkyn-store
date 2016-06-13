0fcd457f94a27d59d40c9e20ea0443383b907b5d
Nicolas Vicente Navarrete Clemente <nicosli@gmail.com>
2016-06-13 09:04:20 -0500

    se comienza la estructura de las tablas de ventas

---
+++ database/migrations/2016_06_13_025029_create_ventas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVentasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ventas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date('fecha_venta');
+            
+            $table->integer('clientes_id')->unsigned();
+            $table->foreign('clientes_id')->references('id')->on('clientes');
+            $table->integer('users_id')->unsigned();
+            $table->foreign('users_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('ventas');
+    }
+}
